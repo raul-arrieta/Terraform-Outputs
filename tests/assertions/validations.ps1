4b4a2ca12e06a54cb896042dd59bf29d7efb0588
Joseph Passineau <joseph.passineau@gsoft.com>
2019-06-28 12:52:09 -0400

    Pass 3rd argument, isSecret, when setting task variable based on if the ouput is sensitive + Added tests

--- tests/assertions/validations.ps1
+++ tests/assertions/validations.ps1
@@ -1,8 +1,20 @@
 param(
     [string]$resourcegroupname = $(throw "-resourcegroupname is required."),
-    [string]$expectedvalue = $(throw "-expectedvalue is required.")
+    [string]$resourcegroupnameexpectedvalue = $(throw "-resourcegroupname is required.")
+    [string]$containerregistryadminusername = $(throw "-containerregistryadminusername is required."),
+    [string]$containerregistryadminusernameexpectedvalue = $(throw "-containerregistryadminusernameexpectedvalue is required.")
+    [string]$containerregistryadminpassword = $(throw "-containerregistryadminpassword is required."),
+    [string]$containerregistryadminpasswordexpectedvalue = $(throw "-containerregistryadminpasswordexpectedvalue is required.")
 )
 
-If ($resourcegroupname -ne $expectedvalue) {
+If ($resourcegroupname -ne $resourcegroupnameexpectedvalue) {
+    throw
+}
+
+If ($containerregistryadminusername -ne $containerregistryadminusernameexpectedvalue) {
+    throw
+}
+
+If ($containerregistryadminpassword -ne $containerregistryadminpasswordexpectedvalue) {
     throw
 }
